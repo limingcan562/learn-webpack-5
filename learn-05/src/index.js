f0f619299f0767b9ce80f545e2e621acc6e8fdd4
limingcan-home <804666207@qq.com>
2023-07-09 15:28:00 +0800

    feat: add "Compile es6+" learning case

---
+++ learn-05/src/index.js
@@ -0,0 +1,12 @@
+class People {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHi() {
+        return Promise.resolve(this.name);
+    }
+}
+
+const Lee = new People('Lee');
+Lee.sayHi().then(name => console.log(`Hi, I am ${name}.`));
