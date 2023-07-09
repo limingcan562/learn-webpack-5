a129c34e81eee100f79ea1470c171650de76cde3
limingcan-home <804666207@qq.com>
2023-07-10 01:49:52 +0800

    feat: Initialize the 'Process HTML' file case

---
+++ learn-06/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './js/[name]-[chunkhash:5].js',
+        assetModuleFilename: './img/[name][ext]',
+        clean: true
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, './dist/[name].html'),
+            template: path.resolve(__dirname, './index.html'),
+            title: 'HtmlWebpackPlugin',
+        })
+    ],
+}
