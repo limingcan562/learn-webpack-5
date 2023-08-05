71981b9829b0a9e2c7f90efcdaa7fed60b5d2aff
limingcan-home <804666207@qq.com>
2023-08-06 01:15:13 +0800

    feat: add "Development Environment Configuration" Cases

--- learn-08/webpack.dev.js
+++ learn-08/webpack.dev.js
@@ -10,7 +10,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name]-[chunkhash:5].js',
-        clean: true
     },
     module: {
         rules: [
